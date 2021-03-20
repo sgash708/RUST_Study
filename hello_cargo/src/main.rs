bdbf0d89ef13813b6a75f68dd5f51fd18903c400
sgash708 <sugash7081023@gmail.com>
2021-03-20 23:41:48 +0900

    while文

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -12,22 +12,28 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input in your guess.");
+    loop {
+        println!("Please input in your guess.");
 
-    // 型推論(mutable)
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess) // read_line method on the standard input handle to get input from the user. 
-        .expect("Failed to read line"); // error handling
-    // String型をスペース削除 && uint32型変換
-    let guess: u32 = guess.trim().parse().expect("Please Type Number!!");
+        // 型推論(mutable)
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess) // read_line method on the standard input handle to get input from the user. 
+            .expect("Failed to read line"); // error handling
+        // String型をスペース削除 && uint32型変換(ParseInt)
+        let guess: u32 = guess.trim().parse().expect("Please Type Number!!");
 
-    // printf("%s-hoge", hoge)みたいな感じ
-    println!("You guessed: {}", guess);
+        // printf("%s-hoge", hoge)みたいな感じ
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!!"),
-        Ordering::Greater => println!("Too big!!"),
-        Ordering::Equal => println!("You win!!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!!"),
+            Ordering::Greater => println!("Too big!!"),
+            Ordering::Equal => {
+                // 正解時処理を抜ける
+                println!("You win!!");
+                break;
+            },
+        }
     }
 }
