82146296d3f5d0594c30fb3a790ec1acc1a40764
sgash708 <sugash7081023@gmail.com>
2021-03-20 23:17:45 +0900

    rand関数について

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,8 +1,15 @@
 // ユーザ入力
 use std::io;
+// Rng (random number generators)
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!!");
+
+    // 長い変数名は"_"でつなぐ
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input in your guess.");
 
     // 型推論(mutable)
@@ -12,5 +19,5 @@ fn main() {
         .expect("Failed to read line"); // error handling
 
     // printf("%s-hoge", hoge)みたいな感じ
-    println!("You guess:{}", guess);
+    println!("You guessed: {}", guess);
 }
